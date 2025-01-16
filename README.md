04acb0ab83a7165d6af77323f3bdb095b4c2f0b0
Ricardo Tavares <ricardo.tavares@viewfromtheweb.com>
2025-01-16 22:26:43 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### svg-loader custom web component
+
+An element that loads SVG code into the DOM so that it can be fully styled, namely by CSS.
+
+
+The demo uses four [Tabler Icons](https://github.com/tabler/tabler-icons)
---
+++ svg-loader.js
@@ -0,0 +1,20 @@
+class SVGLoader extends HTMLElement{
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+        if(this.childNodes.length){ return undefined; }
+        const self = this;
+        const src = this.getAttribute('src');
+        if(!src){ return false; }
+        fetch(src).then(response =>{
+            if (!response.ok){ throw new Error(`HTTP error! status: ${response.status}`); }
+            return response.text();
+        }).then(svg => {
+            const template = document.createElement('template');
+            template.innerHTML = svg;
+            self.appendChild(template.content.cloneNode(true));
+        }).catch(error => { console.error("Error fetching or using SVG:", error); });
+    }
+}
+customElements.define('svg-loader', SVGLoader);
